032dbb2839368b68889de65c422f29ff3bf36b10
yodiodine <yodiodine@gmail.com>
2021-07-27 10:08:28 +0900

    edit

--- js/gift-script.js
+++ js/gift-script.js
@@ -1,3 +1,12 @@
 $(function(){
-    
+    $(window).on("scroll",function(){
+        
+        
+        $(".gift-img1").css("top",(-50+window.scrollY*0.7)+"px");
+        $(".gift-img2").css("top",(500-window.scrollY*0.7)+"px");
+        $(".gift-header h1").css("margin-top",(230+window.scrollY*0.3)+"px")
+        $(".gift-header p").css("left",(-300+window.scrollY*1.4)+"px")
+        
+        
+    })
 })
